80119f09970011e586dd911200023ac374a3d2ce
AleDanish <superalle87@gmail.com>
2016-04-06 17:23:03 +0000

    so

--- bundle/wsgi/database_config.sh
+++ bundle/wsgi/database_config.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 Tstart=$(date +%s%3N) 
 
-#compress and send with one command
-#ssh -i /home/ubuntu/key/mcn-key.pem ubuntu@ip_old 'cd /var/lib/influxdb; sudo tar zcvf - data hh wal meta' > influxdb.backup.tar.gz
-
 if [ "$1" == "" ]; then
    oldfloatingip=$(<../influxdb_oldip)
 else oldfloatingip=$1
@@ -28,7 +25,7 @@ sudo rm -rf /var/lib/influxdb/*
 #extract data
 Textract_start=$(date +%s%3N)
 echo "estraggo i dati"
-sudo tar -xf /home/ubuntu/influxdb.backup.tar -C /var/lib/influxdb
+sudo tar -xf /home/ubuntu/influxdb.backup.tar.gz -C /var/lib/influxdb
 Textract_end=$(date +%s%3N)
 #restart database
 Trestart_start=$(date +%s%3N)
